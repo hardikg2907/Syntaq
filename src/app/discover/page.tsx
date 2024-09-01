54e453bc925093c44f51f35017955babc44d8000
Hardik Garg <hardikg2907@gmail.com>
2024-09-01 16:50:21 +0530

    auth works properly now i guess?

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -5,12 +5,18 @@ import axios from "axios";
 import Link from "next/link";
 import HackathonsCarousel from "~/components/HackathonsCarousel";
 import { BACKEND_API_URL } from "~/utils/constants";
+import { useSession } from "next-auth/react";
 
 const Discover = () => {
+  const { data: session } = useSession();
   const { data: hackathons } = useQuery({
     queryKey: ["hackathons"],
     queryFn: async () => {
-      const response = await axios.get(`${BACKEND_API_URL}/hackathons`);
+      const response = await axios.get(`${BACKEND_API_URL}/hackathons`, {
+        headers: {
+          // Authorization: `Bearer ${session?.access_token}`,
+        },
+      });
       return response.data;
     },
   });
