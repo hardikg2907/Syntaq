ed3026c1dd24f9a37afe7414045b7d6f2edc039f
Hardik Garg <hardikg2907@gmail.com>
2024-09-01 18:36:35 +0530

    used auth function in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
-"use client";
-import { useSession } from "next-auth/react";
+import { auth } from "auth";
 
-export default function HomePage() {
-  const { data: session, status } = useSession();
+export default async function HomePage() {
+  const session = await auth();
   // console.log(JSON.stringify(session, null, 2));
   return (
     <div>
-      {/* Hello */}
-      {status === "loading" && <h2> Loading...</h2>}
       Hello
-      {!(status === "loading") && session && (
+      {session && (
         <>
+          {" "}
           Signed in as {session?.name}
           <br />
         </>
       )}
+      {/* {JSON.stringify(data, null, 2)} */}
     </div>
   );
 }
