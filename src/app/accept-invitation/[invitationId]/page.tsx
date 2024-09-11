5f2ddee357ce506436d86b1f9e05652196d9f6a2
Hardik Garg <hardikg2907@gmail.com>
2024-09-11 14:55:01 +0530

    Refactor accept invitation page and add toast notifications

--- src/app/accept-invitation/[invitationId]/page.tsx
+++ src/app/accept-invitation/[invitationId]/page.tsx
@@ -2,8 +2,11 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import LoadingSpinner from "~/components/LoadingSpinner";
 import SignInPrompt from "~/components/SignInPrompt";
+import { Button } from "~/components/ui/button";
 import { BACKEND_API_URL } from "~/utils/constants";
 
 const AcceptInvitationPage = ({
@@ -13,13 +16,34 @@ const AcceptInvitationPage = ({
 }) => {
   const { data: user } = useSession();
   const invitationId = params.invitationId;
+  const router = useRouter();
 
-  const { isPending, isSuccess, isError, error } = useMutation({
+  const { isPending, isSuccess, isError, error, mutateAsync } = useMutation({
     mutationKey: ["accept-invitation", invitationId],
     mutationFn: async () => {
       const res = await axios.put(
         `${BACKEND_API_URL}/teams/invitations/accept/${invitationId}`,
         {},
+        {
+          headers: {
+            Authorization: `Bearer ${user?.access_token}`,
+          },
+        },
+      );
+      if (!(res.status === 200)) {
+        toast.error("Failed to accept invitation");
+        return;
+      }
+      toast.success("Invitation Accepted");
+      router.push(`/register/${invitation.team.hackathon}`);
+      return res.data;
+    },
+  });
+  const { data: invitation } = useQuery({
+    queryKey: ["invitation", invitationId],
+    queryFn: async () => {
+      const res = await axios.get(
+        `${BACKEND_API_URL}/teams/invitations/${invitationId}`,
         {
           headers: {
             Authorization: `Bearer ${user?.access_token}`,
@@ -36,7 +60,45 @@ const AcceptInvitationPage = ({
         <SignInPrompt message="Sign In to accept this Invitation" />
       </>
     );
-  return <div className="h-full w-full"></div>;
+  if (!invitation) return <div>404 Invitation not found</div>;
+  return (
+    <div className="h-full w-full space-y-8">
+      <h1 className="text-xl font-semibold">
+        Team name: <span>{invitation?.team?.name}</span>
+      </h1>
+      <div className="flex flex-col gap-2">
+        <h3 className="text-xl font-semibold underline">Team Members</h3>
+        <div>
+          {invitation?.team?.members.map((member: any) => (
+            <Member member={member} key={member?.id} />
+          ))}
+        </div>
+      </div>
+      <div className="flex w-full items-center justify-between">
+        <Button
+          onClick={() => mutateAsync()}
+          className="rounded-lg bg-blue-500 p-2"
+        >
+          Accept Invitation
+        </Button>
+        <Button
+          // onClick={() => mutateAsync()}
+          className="rounded-lg bg-red-500 p-2"
+        >
+          Reject
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default AcceptInvitationPage;
+
+const Member = ({ member }: { member: any }) => {
+  return (
+    <div className="flex w-full items-center justify-between rounded-lg border border-gray-800 p-3">
+      {member.userFields.first_name} {member.userFields.last_name}{" "}
+      <span className="text-gray-500">{member.userFields.email}</span>
+    </div>
+  );
+};
